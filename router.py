2a464feab3287d0e43aba176fea3dc1c1aca5e16
Terserah <echbct@gmail.com>
2020-11-30 07:36:13 +0700

    a

--- router.py
+++ router.py
@@ -73,7 +73,7 @@ def clearUs():
 def addRv(rv: Review):
     return{Database().addReview(rv.dict())}
 
-@router.get("/review/find/{username}")
+@router.post("/review/find")
 def findRv(rv: ReviewSUsername):
     return{"reviews": Database().findReview(rv.dict())}
 
